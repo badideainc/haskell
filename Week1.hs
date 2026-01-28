905a992ec899e7b8171deefef8908d29dbda1916
Untitled Programmer <blairlucas448@gmail.com>
2026-01-28 10:32:16 +0000

    Add functions for calculating circumference of a circle and cylinder side, and drinking age validation

---
+++ Week1.hs
@@ -0,0 +1,15 @@
+--1
+
+circumferenceOfCircle :: Float -> Float
+circumferenceOfCircle d = pi * d
+
+sideOfCylinder :: Float -> Float -> Float
+sideOfCylinder d h = circumferenceOfCircle d * h
+
+--2
+
+canDrink :: Int -> Bool
+canDrink age = age >= 18
+
+all3CanDrink :: Int -> Int -> Int -> Bool
+all3CanDrink a b c = canDrink a && canDrink b && canDrink c

6cf7e5f63671352506b0aed79eb0037a0231c902
Untitled Programmer <blairlucas448@gmail.com>
2026-02-20 12:32:43 +0000

    Refactor and reorganize functions in Week4.hs for clarity and consistency

--- Week4.hs
+++ Week4.hs
@@ -3,7 +3,7 @@ import Data.Char
 type StudentMark = (String, Int)
 
 betterStudent :: StudentMark -> StudentMark -> String
-betterStudent (s1,m1) (s2,m2) 
+betterStudent (s1,m1) (s2,m2)
     | m1 >= m2          = s1
     | otherwise         = s2
 
@@ -22,9 +22,45 @@ addPairs :: [(Int,Int)] -> [Int]
 addPairs pairList = [ i+j | (i,j) <- pairList ]
 
 minAndMax :: Int -> Int -> (Int,Int)
-minAndMax x y 
+minAndMax x y
     | x <= y            = (x,y)
     | otherwise         = (y,x)
 
-sumDifference:: Int -> Int -> (Int, Int)
+--1
+
+sumDifference :: Int -> Int -> (Int, Int)
 sumDifference x y = (x + y, x - y)
+
+--2
+
+grade :: StudentMark -> Char
+grade (_, mark)
+    | mark > 100 || mark < 0 = error "Not within the mark range!"
+    | mark >= 70 = 'A'
+    | mark >= 60 = 'B'
+    | mark >= 50 = 'C'
+    | mark >= 40 = 'D'
+    | otherwise = 'F'
+
+--3
+
+capMark :: StudentMark -> StudentMark
+capMark (stu, mark)
+    | mark > 100 || mark < 0 = error "Not within the mark range!"
+    | mark > 40 = (stu, 40)
+    | otherwise = (stu, mark)
+
+--4
+
+firstNumbers :: Int -> [Int]
+firstNumbers x = [1 .. x]
+
+--5
+
+firstSquares :: Int -> [Int]
+firstSquares x = [i ^ 2 | i <- [1.. x]]
+
+--6
+
+capitalise :: String -> String
+capitalise word = [toUpper i | i <- [0 .. word]]
